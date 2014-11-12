202156ee1e48dec2a463a2af30b0b8cd3309adaa
Reptilijonas <galminasgiedrius@gmail.com>
2014-11-12 00:39:25 +0000

    deleted unnecessary?? files

--- bin/AndroidManifest.xml.d
+++
@@ -1,2 +0,0 @@
-C:\Users\Giedrius\Desktop\funfitness\bin\AndroidManifest.xml : \
-C:\Users\Giedrius\Desktop\funfitness\AndroidManifest.xml \
--- bin/FunFitness-release-unsigned.apk.d
+++
@@ -1,5 +0,0 @@
-C:\Users\Giedrius\Desktop\funfitness\bin\FunFitness-release-unsigned.apk : \
-C:\Users\Giedrius\Desktop\funfitness\bin\FunFitness.ap_ \
-C:\Users\Giedrius\Desktop\funfitness\bin\classes.dex \
-C:\Users\Giedrius\Desktop\funfitness\libs\lfopen-api.jar \
-C:\Users\Giedrius\Desktop\funfitness\libs\cordova-2.9.1.jar \
