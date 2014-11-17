69ed8635d25574adda170142f5fdb769b25c42e3
Reptilijonas <galminasgiedrius@gmail.com>
2014-11-17 15:16:19 +0000

    added alerts

--- pluginInterface.js
+++ pluginInterface.js
@@ -1,6 +1,8 @@
 var plugin = {
     test: function(str, successCallback, errorCallback) {
+        alert("pries cordova.exec");
         cordova.exec(successCallback, errorCallback, "Echo", "echo", [str]);
+        alert("po cordova.exec");
     };
 }
 module.exports = plugin;
