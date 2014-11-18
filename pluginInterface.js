9da2bed185dca3085544ee6b7d514699e61cfc3d
Reptilijonas <galminasgiedrius@gmail.com>
2014-11-18 14:29:29 +0000

    sample plugin

--- pluginInterface.js
+++
@@ -1,8 +0,0 @@
-var plugin = {
-    test: function(str, successCallback, errorCallback) {
-        alert("pries cordova.exec");
-        cordova.exec(successCallback, errorCallback, 'Echo', 'echo', [str]);
-        alert("po cordova.exec");
-    };
-}
-module.exports = plugin;
