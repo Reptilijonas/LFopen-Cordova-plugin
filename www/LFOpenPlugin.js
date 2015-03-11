fcf2ba6e3f88c3cdc2919d57bb7e383347b79a46
Reptilijonas <galminasgiedrius@gmail.com>
2015-03-11 02:41:43 +0000

    refactoring

--- www/LFOpenPlugin.js
+++ www/LFOpenPlugin.js
@@ -1,18 +1,12 @@
-var calendar = {
-    createEvent: function(title, location, notes, startDate, endDate, successCallback, errorCallback) {
+var lfopen = {
+    updateWorkoutObj: function(successCallback, errorCallback) {
         cordova.exec(
             successCallback, // success callback function
             errorCallback, // error callback function
-            'Calendar', // mapped to our native Java class called "CalendarPlugin"
-            'addCalendarEntry', // with this action name
-            [{                  // and this array of custom arguments to create our entry
-                "title": title,
-                "description": notes,
-                "eventLocation": location,
-                "startTimeMillis": startDate.getTime(),
-                "endTimeMillis": endDate.getTime()
-            }]
+            'LFOpenPlugin', // mapped to our native Java class called "LFOpenPlugin"
+            'getWorkoutObj', // with this action name
+            []
         ); 
     }
 }
-module.exports = calendar;
+module.exports = lfopen;
